0d238e800279a25dc2f72fd5cbf92ab0e8f75da9
Saad5400 <sdbtwa@gmail.com>
2023-01-13 04:30:42 +0300

    added comments

--- js/uquIndex.js
+++ js/uquIndex.js
@@ -2,6 +2,7 @@
 const java1Card = document.getElementById("java1Card");
 const java1Ul = document.getElementById("java1Ul");
 const liArray = [
+    {text: "Comment Section",       link: "../comments.html",                    apiKey: 11},
     {text: "LAB 1 (شرح)",           link: "JAVA_1_EXP/LAB_1.html#exp",           apiKey: 1},
     {text: "ASSIGNMENT 1 (شرح)",    link: "JAVA_1_EXP/ASSIGNMENT_1.html#exp",    apiKey: 4},
     {text: "LAB 2 (شرح)",           link: "JAVA_1_EXP/LAB_2.html#exp",           apiKey: 6},
@@ -47,7 +48,7 @@ function incrementCounter(key, value = null) {
 
 liArray.forEach(x => {
 
-    let type = x.link.includes("html") ? 1 : 0;
+    let type = x.link.includes("comment") ? 1 : 0;
     let icon = type == 1 ? "skull-outline" : "document-text-outline";
     let swords = x.text.includes("LAB 3") ? '<div class="swordsDiv"><img class="iconSword" src="../images/swords.png"></div>' : ""
 
